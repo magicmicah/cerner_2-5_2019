1399f336e104f19a6f5abe4082bf859227c08b40
Micah Magruder <mmagruder@gmail.com>
2019-09-13 22:16:58 -0400

    Update Invoke-HunterConfirmation.ps1

--- Invoke-HunterConfirmation.ps1
+++ Invoke-HunterConfirmation.ps1
@@ -1,10 +1,15 @@
-﻿function Invoke-HunterConfirmation {
+# Invoke-HunterConfirmation
+# I bought a Hunter Ceiling Fan with smart control features. The app is stuck in a loop on sign-in by confirming your email. I read online that other people with similar issues encountered similar issues. One person de-compiled the apk and found a way to submit a json token to get it to work. He used postman, but I chose to use PowerShell. It worked and I can get into the app now. If I could improve upon this in any way, I would add error handling but for a simple PUT to a rest endpoint, this did the trick.
+# https://davidmburke.com/2019/06/01/how-to-set-up-a-hunter-fan-wifi-control-by-decompiling-their-app/
+# cerner_2^5_2019
+
+function Invoke-HunterConfirmation {
   param(
     $url = "https://user.aylanetworks.com/users/confirmation.json",
     $token
   )
 
-  $tokenToJson = @{confirmation_token=$token} |ConvertTo-Json
+  $tokenToJson = @{confirmation_token=$token} | ConvertTo-Json
 
   Invoke-RestMethod -Uri $url -Method Put -Body $tokenToJson -ContentType 'application/json'
 
