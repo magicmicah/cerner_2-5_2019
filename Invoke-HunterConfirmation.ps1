f3e170a65cd424a0c5e788517c537ee3840f6939
Micah Magruder <mmagruder@gmail.com>
2019-09-13 22:10:57 -0400

    Invoke-HunterConfirmation.ps1

---
+++ Invoke-HunterConfirmation.ps1
@@ -0,0 +1,13 @@
+﻿function Invoke-HunterConfirmation {
+  param(
+    $url = "https://user.aylanetworks.com/users/confirmation.json",
+    $token
+  )
+
+  $tokenToJson = @{confirmation_token=$token} |ConvertTo-Json
+
+  Invoke-RestMethod -Uri $url -Method Put -Body $tokenToJson -ContentType 'application/json'
+
+}
+
+Invoke-HunterConfirmation -token $Args[0]
